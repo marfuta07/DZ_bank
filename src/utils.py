b35e06c44854485ec55f856a555f38d5177eb82a
Юлия Бусыгина <ubusygina747@gmail.com>
2026-04-21 14:44:18 +0500

    Внесены изменения

--- src/utils.py
+++ src/utils.py
@@ -9,18 +9,24 @@ def load_transactions_from_json(file_path: str) -> List[Dict]:
     с данными о финансовых транзакциях.
     Если файл пустой, содержит не список или не найден, функция возвращает пустой список
     """
+
     # Проверяем, существует ли файл
     if not os.path.exists(file_path):
+
         return []
 
     try:
         with open(file_path, "r", encoding="utf-8") as file:
+
             data = json.load(file)
+
 
             # Проверяем, является ли данные списком
             if isinstance(data, list):
+
                 return data
             else:
+
                 return []
     except json.JSONDecodeError:
         # Если файл пустой или содержит некорректный JSON
